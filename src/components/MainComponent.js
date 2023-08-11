39df7fa28fb2a06481feb795e37681a6508342f6
Aniket <ghoshaniket1000@gmail.com>
2023-08-11 07:38:55 +0530

    modifying app.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import GitHubButton from './GitHubButton';
-import TwitterButton from './TwitterButton';
-import LinkedinButton from './LinkedinButton';
-import HashnodeButton from './HashnodeButton';
-import InstagramButton from './InstagramButton';
-import MailButton from './Mail';
+import SocialMediaButton from './SocialMediaButton';
+
 const MainComponent = () => {
   return (
     <main style={{ paddingTop: '80px' }} className="circles">
       <SocialMediaButton link="https://github.com/aniketh3014" text="GitHub" />
       <SocialMediaButton link="https://twitter.com/aniketh3014" text="Twitter" />
-      
+      <SocialMediaButton link="https://www.linkedin.com/in/aniket-ghosh-8b0b53281/" text="Linkedin" />
+      <SocialMediaButton link="https://hashnode.com/@aniketh3014" text="Hashnode" />
+      <SocialMediaButton link="https://www.instagram.com/aniket_ghosh440/" text="Instagram" />
     </main>
   );
 };
